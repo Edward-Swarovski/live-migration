79038ae6916cbd453bbabcdcccaeb4703eb77042
Edward-Swarovski <34472521+Edward-Swarovski@users.noreply.github.com>
2025-05-29 15:43:13 +0100

    Create gen_drop_nt_service_dbuser.sql

---
+++ gen_drop_nt_service_dbuser.sql
@@ -0,0 +1,36 @@
+SET NOCOUNT ON;
+
+DECLARE @dbName NVARCHAR(256), @sql NVARCHAR(MAX);
+
+DECLARE db_cursor CURSOR FOR
+SELECT name FROM sys.databases
+WHERE database_id > 4 AND state_desc = 'ONLINE'; -- Exclude system DBs
+
+OPEN db_cursor;
+FETCH NEXT FROM db_cursor INTO @dbName;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @sql = '
+    USE [' + @dbName + '];
+
+    DECLARE @dropSQL NVARCHAR(MAX) = '''';
+
+    SELECT @dropSQL = @dropSQL +
+        ''USE ['' + DB_NAME() + '']; DROP USER ['' + dp.name + ''];'' + CHAR(13) + CHAR(10)
+    FROM sys.database_principals dp
+    WHERE dp.name IN (
+        ''NT SERVICE\MSSQLSERVER'',
+        ''NT SERVICE\SQLSERVERAGENT''
+    );
+
+    SELECT @dropSQL AS DropStatements;
+    ';
+
+    EXEC sp_executesql @sql;
+
+    FETCH NEXT FROM db_cursor INTO @dbName;
+END
+
+CLOSE db_cursor;
+DEALLOCATE db_cursor;
