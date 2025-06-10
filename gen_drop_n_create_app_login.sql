dc116ebc169690f21e1d99b0afc0fe0f90bee75d
Edward-Swarovski <34472521+Edward-Swarovski@users.noreply.github.com>
2025-06-10 11:36:55 +0100

    Update gen_drop_n_create_app_login.sql

--- gen_drop_n_create_app_login.sql
+++ gen_drop_n_create_app_login.sql
@@ -1,28 +1,71 @@
 USE master;
 GO
 
--- Create a temporary table to store login details
+-- Create temp table to store all desired login details
 IF OBJECT_ID('tempdb..#Logins') IS NOT NULL
     DROP TABLE #Logins;
 
 CREATE TABLE #Logins (
     LoginName NVARCHAR(128),
     SID VARBINARY(85),
+    TypeDesc NVARCHAR(60),
     PasswordHash VARBINARY(256),
     DefaultDatabase NVARCHAR(128),
     IsDisabled BIT
 );
 
--- Insert SQL Server logins into the temp table (excluding system and excluded logins)
-INSERT INTO #Logins (LoginName, SID, PasswordHash, DefaultDatabase, IsDisabled)
+-- Insert SQL-authenticated logins
+INSERT INTO #Logins (LoginName, SID, TypeDesc, PasswordHash, DefaultDatabase, IsDisabled)
 SELECT 
-    name AS LoginName,
-    sid AS SID,
-    password_hash AS PasswordHash,
-    default_database_name AS DefaultDatabase,
-    is_disabled AS IsDisabled
+    name,
+    sid,
+    type_desc,
+    password_hash,
+    default_database_name,
+    is_disabled
 FROM sys.sql_logins
-WHERE type = 'S'
+WHERE name NOT IN (
+    '##MS_PolicyEventProcessingLogin##',
+    '##MS_PolicyTsqlExecutionLogin##',
+    'a2pdba',
+    'a2psysro',
+    'BUILTIN\Administrators',
+    'dbadb_dbo',
+    'dbmsmon',
+    'dbtools_copydb',
+    'dbtools_restoredb',
+    'dbtoolsp',
+    'delphix_admin',
+    'hp_dbspi',
+    'infosec',
+    'IT_SEC_ADMIN',
+    'mdscout',
+    'NT AUTHORITY\NETWORK SERVICE',
+    'NT AUTHORITY\SYSTEM',
+    'NT SERVICE\MSSQLSERVER',
+    'NT SERVICE\SQLSERVERAGENT',
+    'NT SERVICE\SQLTELEMETRY',
+    'NT SERVICE\SQLWriter',
+    'NT SERVICE\Winmgmt',
+    'PRODUCTION\DBSysAdminAll-ACC-G',
+    'PRODUCTION\sys-a2pdba1-g',
+    'PRODUCTION\sys-a2pdba2-g',
+    'QUALYS_DB',
+    'sa',
+    'sa_maint'
+);
+
+-- Insert Windows-authenticated logins
+INSERT INTO #Logins (LoginName, SID, TypeDesc, PasswordHash, DefaultDatabase, IsDisabled)
+SELECT 
+    name,
+    sid,
+    type_desc,
+    NULL AS password_hash,
+    NULL AS default_database_name,
+    is_disabled
+FROM sys.server_principals
+WHERE type_desc = 'WINDOWS_LOGIN'
 AND name NOT IN (
     '##MS_PolicyEventProcessingLogin##',
     '##MS_PolicyTsqlExecutionLogin##',
@@ -54,16 +97,21 @@ AND name NOT IN (
     'sa_maint'
 );
 
--- Generate DROP and CREATE LOGIN statements
+-- Generate drop and recreate login statements
 SELECT 
     'BEGIN TRY ' +
-        'IF EXISTS (SELECT * FROM sys.sql_logins WHERE name = ''' + LoginName + ''') ' +
+        'IF EXISTS (SELECT * FROM sys.server_principals WHERE name = ''' + LoginName + ''') ' +
         'DROP LOGIN [' + LoginName + ']; ' +
-        'CREATE LOGIN [' + LoginName + '] ' +
-        'WITH PASSWORD = ' + CONVERT(VARCHAR(MAX), PasswordHash, 1) + ' HASHED, ' +
-        'SID = ' + CONVERT(VARCHAR(MAX), SID, 1) + ', ' +
-        'DEFAULT_DATABASE = [' + DefaultDatabase + '], ' +
-        'CHECK_POLICY = OFF; ' +
+        CASE 
+            WHEN TypeDesc = 'SQL_LOGIN' THEN 
+                'CREATE LOGIN [' + LoginName + '] ' +
+                'WITH PASSWORD = ' + CONVERT(VARCHAR(MAX), PasswordHash, 1) + ' HASHED, ' +
+                'SID = ' + CONVERT(VARCHAR(MAX), SID, 1) + 
+                ISNULL(', DEFAULT_DATABASE = [' + DefaultDatabase + ']', '') + ', CHECK_POLICY = OFF; '
+            WHEN TypeDesc = 'WINDOWS_LOGIN' THEN 
+                'CREATE LOGIN [' + LoginName + '] FROM WINDOWS WITH SID = ' + CONVERT(VARCHAR(MAX), SID, 1) + '; '
+            ELSE '-- Skipped unsupported type: ' + TypeDesc
+        END +
         CASE WHEN IsDisabled = 1 THEN 'ALTER LOGIN [' + LoginName + '] DISABLE; ' ELSE '' END +
     'END TRY ' +
     'BEGIN CATCH ' +
