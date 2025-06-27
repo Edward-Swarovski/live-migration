e5b558d8a765e686292533b3d05e8e57b4becc83
Edward-Swarovski <34472521+Edward-Swarovski@users.noreply.github.com>
2025-06-27 13:49:38 +0100

    Update pre-prod-capture_dbuser.sh

--- pre-prod-capture_dbuser.sh
+++ pre-prod-capture_dbuser.sh
@@ -17,23 +17,8 @@ fi
 
 echo "Starting SQL operations on server ${MSSQL_SVRNAME}..."
 
-# Generate / Execute SQL to change schema ownership to DBO
-echo "Changing schema ownership to DBO..."
-sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i gen_chg_schema_owner_to_dbo.sql -w9999 -o ${MSSQL_SVRNAME}_chg_schema_owner_to_dbo.sql -h -1 -W -P `cat sa_maint.pwd`
-sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i ${MSSQL_SVRNAME}_chg_schema_owner_to_dbo.sql -w9999 -o ${MSSQL_SVRNAME}_chg_schema_owner_to_dbo.out -h -1 -W -P `cat sa_maint.pwd`
-
-# Generate / Execute SQL to remove orphan user [NT *****\ USER]
-echo "Removing NT service DB users..."
-sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i gen_drop_nt_service_dbuser.sql -w9999 -o ${MSSQL_SVRNAME}_drop_nt_service_dbuser.sql -h -1 -W -P `cat sa_maint.pwd`
-sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i ${MSSQL_SVRNAME}_drop_nt_service_dbuser.sql -w9999 -o ${MSSQL_SVRNAME}_drop_nt_service_dbuser.out -h -1 -W -P `cat sa_maint.pwd`
-
-# Generate / Execute SQL to remove orphan user
-echo "Removing orphan DB users..."
-sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i gen_drop_orphan_dbuser.sql -w9999 -o ${MSSQL_SVRNAME}_drop_orphan_dbuser.sql -h -1 -W -P `cat sa_maint.pwd`
-sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i ${MSSQL_SVRNAME}_drop_orphan_dbuser.sql -w9999 -o ${MSSQL_SVRNAME}_drop_orphan_dbuser.out -h -1 -W -P `cat sa_maint.pwd`
-
 # Generate SQL to change database file autogrowth
-echo "Generate Alter database autogrowh statements..."
+echo "Generating ALTER DATABASE AUTOGROWTH statements..."
 sqlcmd -S${MSSQL_SVRNAME},2500 -U sa_maint -i gen_alter_db_autogrowth.sql -w9999 -o ${MSSQL_SVRNAME}_alter_db_autogrowth.sql -h -1 -W -P `cat sa_maint.pwd`
 
 # Generate CREATE USER statement
